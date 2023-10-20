29b4220d00e61d16cc3dab0b655ed66dd07710e3
Usr_Prime <>
2023-10-20 11:34:25 -0300

    Finish interface

--- CambioApp/CambioApp/Views/RatesFluctuationView.swift
+++ CambioApp/CambioApp/Views/RatesFluctuationView.swift
@@ -123,23 +123,25 @@ struct RatesFluctuationView: View {
     
     private var ratesFluctuationListView: some View {
         List(searchResult) { fluctuation in
-            VStack {
-                HStack(alignment: .center, spacing: 8) {
-                    Text("\(fluctuation.symbol) / BRL")
-                        .font(.system(size: 14, weight: .medium))
-                    Text(fluctuation.endRate.formatter(decimalPlaces: 2))
-                        .font(.system(size: 14, weight: .bold))
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                    Text(fluctuation.change.formatter(decimalPlaces: 4, with: true))
-                        .font(.system(size: 14, weight: .bold))
-                        .foregroundColor(fluctuation.change.color)
-                    Text("(\(fluctuation.changePct.toPercentage()))")
-                        .font(.system(size: 14, weight: .bold))
-                        .foregroundColor(fluctuation.change.color)
+            NavigationLink(destination: RateFluctuationDetailView(rateFluctuation: fluctuation, baseCurrency: "BRL")) {
+                VStack {
+                    HStack(alignment: .center, spacing: 8) {
+                        Text("\(fluctuation.symbol) / BRL")
+                            .font(.system(size: 14, weight: .medium))
+                        Text(fluctuation.endRate.formatter(decimalPlaces: 2))
+                            .font(.system(size: 14, weight: .bold))
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                        Text(fluctuation.change.formatter(decimalPlaces: 4, with: true))
+                            .font(.system(size: 14, weight: .bold))
+                            .foregroundColor(fluctuation.change.color)
+                        Text("(\(fluctuation.changePct.toPercentage()))")
+                            .font(.system(size: 14, weight: .bold))
+                            .foregroundColor(fluctuation.change.color)
+                    }
+                    Divider()
+                        .padding(.leading, -20)
+                        .padding(.trailing, -40)
                 }
-                Divider()
-                    .padding(.leading, -20)
-                    .padding(.trailing, -40)
             }
             .listRowSeparator(.hidden)
             .listRowBackground(Color.white)
